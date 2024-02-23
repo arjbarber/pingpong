f49872123979b3ec300e179e41a2cf9a29347933
Janielle Goolgar <Janielle.Goolgar@lexisnexisrisk.com>
2024-02-22 23:21:15 -0500

    added ball

--- main.py
+++ main.py
@@ -6,13 +6,17 @@ pygame.init()
 
 WIN = pygame.display.set_mode((config.WIDTH,config.HEIGHT))
 
-def draw_screen(p1,p2):
+def draw_screen(p1,p2,ball):
     WIN.fill(colors.BLACK)
 
     pygame.draw.rect(WIN, colors.RED, p1)
     pygame.draw.rect(WIN, colors.RED, p2)
-    pygame.draw.circle(WIN, colors.WHITE,(config.WIDTH/2,config.HEIGHT/2),5,width=0)
+    pygame.draw.circle(WIN, colors.WHITE,(ball.x + config.BALL_RADIUS,ball.y + config.BALL_RADIUS),config.BALL_RADIUS,width=0)
     pygame.display.update()
+
+def ball_movement(ball, p1, p2):
+    if pygame.Rect.colliderect(ball):
+        
 
 def p1_movement(keys_pressed, p1):
     if keys_pressed[pygame.K_w] and p1.y - config.PLAYER_VEL >= 0:
@@ -29,6 +33,7 @@ def p2_movement(keys_pressed, p2):
 def main():
     p1 = pygame.Rect(20, config.HEIGHT/2, config.PLAYER_WIDTH, config.PLAYER_HEIGHT)
     p2 = pygame.Rect(config.WIDTH-20, config.HEIGHT/2, config.PLAYER_WIDTH, config.PLAYER_HEIGHT)
+    ball = pygame.Rect(config.WIDTH/2,config.HEIGHT/2, config.BALL_RADIUS*2, config.BALL_RADIUS*2)
 
     clock = pygame.time.Clock()
     run = True
@@ -41,7 +46,7 @@ def main():
         keys_pressed = pygame.key.get_pressed()
         p1_movement(keys_pressed, p1)
         p2_movement(keys_pressed, p2)
-        draw_screen(p1,p2)
+        draw_screen(p1,p2,ball)
 
     pygame.quit()
     sys.exit()
